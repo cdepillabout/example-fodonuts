a35926e21b74dc2706148b21a31f62520d8cb7ab
(cdep)illabout <cdep.illabout@gmail.com>
2023-07-19 13:26:47 +0900

    update

--- nix/fod-example-simple/default.nix
+++ nix/fod-example-simple/default.nix
@@ -2,4 +2,15 @@
 { stdenv }:
 
 stdenv.mkDerivation {
+  name = "fod-example-simple";
+
+  # This is the sha256 hash for the string "hello world", which is output upon
+  # this derivation building.
+  outputHash = "sha256-gbK9TqmMjbZlVPvI12N6GmmhMPMx/rcyt1yqtMSGj9U=";
+  outputHashMode = "flat";
+  outputHashAlgo = "sha256";
+
+  buildCommand = ''
+    echo 'hello world' > $out
+  '';
 }
